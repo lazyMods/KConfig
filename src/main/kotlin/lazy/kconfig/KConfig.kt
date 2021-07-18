2caa2be15844079caf2de8258620e39a6783995f
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-07-19 00:21:14 +0100

    Main class that holds the config utilities like creating ConfigHolder

---
+++ src/main/kotlin/lazy/kconfig/KConfig.kt
@@ -0,0 +1,25 @@
+package lazy.kconfig
+
+import java.nio.file.Path
+
+object KConfig {
+
+    private var loadedConfigs = listOf<ConfigEntry>()
+
+    fun init(filePath: Path) {
+        loadedConfigs = ConfigParser.load(filePath)
+    }
+
+    fun <T> createConfigHolder(key: String): ConfigHolder<T> {
+        return ConfigHolder(getByKey(key)!!.value as T)
+    }
+
+    private fun getByKey(key: String): ConfigEntry? {
+        loadedConfigs.runCatching {
+            return first { it.key == key }
+        }.onFailure {
+            println("Couldn't find key: $key")
+        }
+        return null
+    }
+}
