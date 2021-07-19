322962cf9f442b017de9611632576c025394ec73
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-07-19 19:57:10 +0100

    Improved message when the key isn't available

--- src/main/kotlin/lazy/kconfig/KConfig.kt
+++ src/main/kotlin/lazy/kconfig/KConfig.kt
@@ -4,21 +4,22 @@ import java.nio.file.Path
 
 object KConfig {
 
+    lateinit var id: String
     private var loadedConfigs = listOf<ConfigEntry>()
 
     fun init(filePath: Path) {
+        id = filePath.fileName.toString()
         loadedConfigs = ConfigParser.parse(filePath)
     }
 
-    fun <T> createConfigHolder(key: String): ConfigHolder<T> {
-        return ConfigHolder(getByKey(key)!!.value as T)
-    }
+    @Suppress("UNCHECKED_CAST")
+    fun <T> createConfigHolder(key: String): ConfigHolder<T> = ConfigHolder(getByKey(key)!!.value as T)
 
     private fun getByKey(key: String): ConfigEntry? {
         loadedConfigs.runCatching {
             return first { it.key == key }
         }.onFailure {
-            println("Couldn't find key: $key")
+            println("[KConfig] Couldn't find key: --> $key <--. Config File: $id")
         }
         return null
     }
