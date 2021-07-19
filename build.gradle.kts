d830bfa3ad5456fb1b7b1ca9d97a651248a4de81
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-07-19 11:02:59 +0100

    now publishing to github packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.konan.properties.Properties
 
 plugins {
     kotlin("jvm") version "1.5.10"
+    `maven-publish`
 }
 
 group = "lazy"
@@ -21,4 +23,26 @@ tasks.test {
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+publishing {
+    val propertiesFile = file("local.properties")
+    val properties = Properties()
+    properties.load(propertiesFile.inputStream())
+
+    repositories {
+        maven {
+            name = "lazyGithubPackages"
+            url = uri("https://maven.pkg.github.com/lazyMods/k-config")
+            credentials {
+                username = properties.getProperty("gpr.user") as String
+                password = properties.getProperty("gpr.key") as String
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
