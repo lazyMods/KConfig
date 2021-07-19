38dc9bd36787ec4bdf3ceb102fb4297da3d568c9
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-07-19 01:24:22 +0100

    Removed Min and Max Values since the user shouldn't change them

--- src/test/kotlin/lazy/kconfig/Tests.kt
+++ src/test/kotlin/lazy/kconfig/Tests.kt
@@ -6,33 +6,38 @@ import kotlin.test.assertEquals
 
 class Tests {
 
-    private val load = ConfigParser.load(Paths.get("test.props"))
+    private val load = ConfigParser.parse(Paths.get("test.toml"))
 
     @Test
     fun containsConfig() {
-        assertEquals(true, load.any { it.key == "fooIsBar" })
+        assertEquals(true, load.any { it.key == "pineappleOnPizza" })
         assertEquals(true, load.any { it.key == "name" })
+        assertEquals(true, load.any { it.key == "height" })
+        assertEquals(true, load.any { it.key == "age" })
     }
 
     @Test
     fun configHolder() {
-        KConfig.init(Paths.get("test.props"))
-        val configHolder = KConfig.createConfigHolder<Int>("minMax")
-        assertEquals(10, configHolder.get())
+        KConfig.init(Paths.get("test.toml"))
+        val age = KConfig.createConfigHolder<Int>("age")
+        val name = KConfig.createConfigHolder<String>("name")
+        val pineappleOnPizza = KConfig.createConfigHolder<Boolean>("pineappleOnPizza")
+        val height = KConfig.createConfigHolder<Double>("height")
+        assertEquals(23, age.get())
+        assertEquals("Marco", name.get())
+        assertEquals(false, pineappleOnPizza.get())
+        assertEquals(1.69, height.get())
     }
 
     @Test
-    fun hasMinMax() {
-        assertEquals(1, load.first { it.key == "minMax" }.min)
-        assertEquals(10, load.first { it.key == "minMax" }.max)
-    }
-
-    @Test
-    fun booleanTest() = assertEquals(false, load.first { it.key == "fooIsBar" }.value)
+    fun booleanTest() = assertEquals(false, load.first { it.key == "pineappleOnPizza" }.value)
 
     @Test
     fun stringTest() = assertEquals("Marco", load.first { it.key == "name" }.value)
 
     @Test
-    fun intTest() = assertEquals(10, load.first { it.key == "minMax" }.value)
+    fun intTest() = assertEquals(23, load.first { it.key == "age" }.value)
+
+    @Test
+    fun doubleTest() = assertEquals(1.69, load.first { it.key == "height" }.value)
 }
