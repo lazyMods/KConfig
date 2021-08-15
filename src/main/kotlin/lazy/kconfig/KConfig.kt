c5135110297522e3f547b665d2111f8dfafe0891
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-08-15 23:14:25 +0100

    Added ranged entries that return the default value if the loaded values isnt in range

--- src/main/kotlin/lazy/kconfig/KConfig.kt
+++ src/main/kotlin/lazy/kconfig/KConfig.kt
@@ -10,22 +10,40 @@ object KConfig {
 
     fun init(filePath: Path) {
         id = filePath.fileName.toString()
-        if(!Files.exists(filePath)){
+        if (!Files.exists(filePath)) {
             println("[KConfig] Creating config file with id: $id")
             Files.createFile(filePath)
         }
         loadedConfigs = ConfigParser.parse(filePath)
     }
 
-    @Suppress("UNCHECKED_CAST")
+    fun createIntRangeConfigHolder(key: String, defaultValue: Int, range: Pair<Int, Int>): ConfigHolder<Int> {
+        val holder = internalCreateConfigHolder(key, defaultValue, "Ranged Value: Greater than ${range.first} and less than ${range.second}")
+        if (isInRangeInt(range, holder.get())) return holder
+        println("[KConfig] Loaded value isn't in range! [${holder.get()}] Creating holder with default value. [$defaultValue]")
+        return ConfigHolder(defaultValue)
+    }
+
+    fun createDoubleRangeConfigHolder(key: String, defaultValue: Double, range: Pair<Double, Double>): ConfigHolder<Double> {
+        val holder = internalCreateConfigHolder(key, defaultValue, "Ranged Value: Greater than ${range.first} and less than ${range.second}")
+        if (isInRangeDouble(range, holder.get())) return holder
+        println("[KConfig] Loaded value isn't in range! [${holder.get()}] Creating holder with default value. [$defaultValue]")
+        return if (isInRangeDouble(range, holder.get())) return holder else ConfigHolder(defaultValue)
+    }
+
     fun <T> createConfigHolder(key: String, defaultValue: T): ConfigHolder<T> {
-        if(getByKey(key) == null){
+        return internalCreateConfigHolder(key, defaultValue, "")
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <T> internalCreateConfigHolder(key: String, defaultValue: T, comment: String): ConfigHolder<T> {
+        if (getByKey(key) == null) {
             println("[KConfig] Creating entry with key: --> $key <-- value: $defaultValue. Config File: $id")
-            ConfigParser.addConfigEntry(ConfigEntry(key, defaultValue))
+            ConfigParser.addConfigEntry(ConfigEntry(key, defaultValue), comment)
             return ConfigHolder(defaultValue)
         }
         val fromKey = ConfigHolder(getByKey(key)!!.value as T)
-        if(defaultValue != fromKey.get())
+        if (defaultValue != fromKey.get())
             println("[KConfig] Loaded $key from file with value: ${fromKey.get()} [Default is $defaultValue]")
         return fromKey
     }
@@ -38,4 +56,9 @@ object KConfig {
         }
         return null
     }
+
+    private fun isInRangeInt(range: Pair<Int, Int>, value: Int): Boolean = containsInt(value, range)
+    private fun isInRangeDouble(range: Pair<Double, Double>, value: Double): Boolean = containsDouble(value, range)
+    private fun containsInt(value: Int, range: Pair<Int, Int>): Boolean = range.first <= value && value <= range.second
+    private fun containsDouble(value: Double, range: Pair<Double, Double>): Boolean = range.first <= value && value <= range.second
 }
